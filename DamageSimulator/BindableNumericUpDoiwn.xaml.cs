46c6233ce82c8667b94757c2edf133d0fdb75332
YSRKEN <ysr.ken@gmail.com>
2016-07-31 12:04:13 +0900

    対潜攻撃について、ヒストグラムを表示できるようになった

---
+++ DamageSimulator/BindableNumericUpDoiwn.xaml.cs
@@ -0,0 +1,79 @@
+﻿/* WPFでNumericUpDownを使う _ tocsworld
+ * https://tocsworld.wordpress.com/2014/05/10/wpf%E3%81%A7numericupdown%E3%82%92%E4%BD%BF%E3%81%86/
+ */
+namespace BindableWinFormsControl
+{
+	using System;
+	using System.Windows;
+	using System.ComponentModel;
+	using System.Windows.Forms;
+	using System.Windows.Forms.Integration;
+	/// <summary>
+	/// BindableNumericUpDown.xaml の相互作用ロジック
+	/// </summary>
+	public partial class BindableNumericUpDown : WindowsFormsHost, INotifyPropertyChanged
+	{
+		#region 依存関係プロパティ
+		public static readonly DependencyProperty ValueProperty;
+		static BindableNumericUpDown()
+		{
+			BindableNumericUpDown.ValueProperty = DependencyProperty.Register(
+				"Value",
+				typeof(Decimal),
+				typeof(BindableNumericUpDown),
+				new FrameworkPropertyMetadata(0m,
+											  FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+											  new PropertyChangedCallback(OnValueChanged)));
+		}
+		public Decimal Value
+		{
+			// このプロパティはコンパイル時に参照されるが、実行時には参照されない。
+			// そのためこの.NETプロパティラッパにロジックを入れてはいけない。
+			get { return (Decimal)GetValue(BindableNumericUpDown.ValueProperty); }
+			set {
+				SetValue(BindableNumericUpDown.ValueProperty, value);
+				NotifyPropertyChanged("Value");
+			}
+		}
+		#endregion
+ 
+		private static void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+		{
+			BindableNumericUpDown control = sender as BindableNumericUpDown;
+			if (control == null) {
+				return;
+			}
+			if (e.Property == ValueProperty)
+			{
+				control.Value = (Decimal)e.NewValue;
+			}
+		}
+ 
+		public BindableNumericUpDown()
+		{
+			InitializeComponent();
+			SetUpBind();
+		}
+ 
+		#region 表示と依存関係プロパティの同期
+		/// <summary>
+		/// bypassData と NumericUpDown.Value のバインド
+		/// </summary>
+		private void SetUpBind()
+		{
+			var binding2 = new BindingSource();
+			((ISupportInitialize)binding2).BeginInit();
+			NumericUpDown child = Child as NumericUpDown;
+			child.DataBindings.Add(new System.Windows.Forms.Binding("Value", binding2, "Value", true, DataSourceUpdateMode.OnPropertyChanged));
+			binding2.DataSource = typeof(BindableNumericUpDown);
+			((ISupportInitialize)binding2).EndInit();
+			binding2.DataSource = this;
+		}
+		private void NotifyPropertyChanged(string propertyName)
+		{
+			PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+		public event PropertyChangedEventHandler PropertyChanged = (s, e) => { };
+		#endregion
+	}
+}
--- DamageSimulator/MainWindow.xaml.cs
+++ DamageSimulator/MainWindow.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms.Integration;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -18,13 +20,329 @@ namespace DamageSimulator {
 	/// MainWindow.xaml の相互作用ロジック
 	/// </summary>
 	public partial class MainWindow : Window {
+		/* メンバ変数 */
+		Chart chart;
+		SortedDictionary<int, int> sorted_hist;
+		System.Random rand = new System.Random();
+		bool autoCalcFlg = false;
+		const int TabIndexAntiSub = 1;
+		int[] loopCount = { 1000, 10000, 100000, 1000000 };
+
+		/* メソッド */
 		public MainWindow() {
 			InitializeComponent();
-			DataContext = new { CriticalLabelString = "13.3%" };
+			// チャート用の初期設定
+			var windowsFormsHost1 = (WindowsFormsHost)grid_Graph.Children[0];
+			chart = (Chart)windowsFormsHost1.Child;
+			chart.ChartAreas.Add("ChartArea");
+			DataContext = new { CriticalLabelString = "13.3%"};
 		}
 
+		/// <summary>
+		/// スライドバーを動かした際の処理
+		/// </summary>
 		private void slider_Critical_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
-			DataContext = new { CriticalLabelString = "" + (slider_Critical.Value / 10).ToString("0.0") + "%" };
+			DataContext = new { CriticalLabelString = "" + (slider_Critical.Value / 10).ToString("0.0") + "%"};
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		/// <summary>
+		/// 「計算開始」ボタンを押した際の処理
+		/// </summary>
+		private void button_Run_Click(object sender, RoutedEventArgs e) {
+			CalcHistogram();
+		}
+
+		/// <summary>
+		/// ヒストグラムの計算処理
+		/// </summary>
+		private void CalcHistogram() {
+			try {
+				// 基本攻撃力を算出する
+				var baseAttackValue = 0.0;
+				if(tabControl.SelectedIndex == TabIndexAntiSub) {
+					// 素対潜
+					baseAttackValue += Math.Sqrt(int.Parse(textBox_AntiSub_Kammusu.Text)) * 2;
+					// 装備対潜
+					baseAttackValue += 1.5 * int.Parse(textBox_AntiSub_Weapons.Text);
+					// 装備改修値
+					if(comboBox_AntiSub.SelectedIndex == 0) {
+						baseAttackValue += Math.Sqrt(comboBox_AntiSub_Level_0.SelectedIndex);
+						baseAttackValue += Math.Sqrt(comboBox_AntiSub_Level_1.SelectedIndex);
+						baseAttackValue += Math.Sqrt(comboBox_AntiSub_Level_2.SelectedIndex);
+						baseAttackValue += Math.Sqrt(comboBox_AntiSub_Level_3.SelectedIndex);
+					}
+					// 対潜定数
+					baseAttackValue += (comboBox_AntiSub.SelectedIndex == 0 ? 13 : 8);
+				}
+				// キャップ前攻撃力を出す
+				var attackValueBeforeCap = baseAttackValue;
+				//対潜シナジー補正
+				if(tabControl.SelectedIndex == TabIndexAntiSub) {
+					if((bool)checkBox_AntiSubSynergy.IsChecked)
+						attackValueBeforeCap *= 1.15;
+				}
+				//陣形補正
+				if(tabControl.SelectedIndex == TabIndexAntiSub) {
+					double[] param = { 0.6, 0.8, 1.2, 1.0, 1.3 };
+					attackValueBeforeCap *= param[comboBox_Formation.SelectedIndex];
+				}
+				//損傷補正
+				if(tabControl.SelectedIndex == TabIndexAntiSub) {
+					double[] param = { 1, 0.7, 0.4 };
+					attackValueBeforeCap *= param[comboBox_Damage.SelectedIndex];
+				}
+				//交戦形態補正
+				{
+					// 「彩雲有り」の場合、同航戦：反航戦：丁字有利：丁字不利＝45：40：15：0
+					// 「彩雲無し」の場合、同航戦：反航戦：丁字有利：丁字不利＝45：30：15：10
+					// したがって、
+					double[] param = { 1, 0.8, 1.2, 0.6 };
+					attackValueBeforeCap *= param[comboBox_Position.SelectedIndex];
+				}
+				// キャップ後攻撃力を出す
+				var attackValueAfterCap = attackValueBeforeCap;
+				if(tabControl.SelectedIndex == TabIndexAntiSub) {
+					if(attackValueAfterCap > 100.0)
+						attackValueAfterCap = 100.0 + Math.Sqrt(attackValueAfterCap - 100.0);
+				}
+				// 最終攻撃力を出す
+				double lastAttackValueWithoutCL = (int)attackValueAfterCap;
+				var lastAttackValueWithCL_ = (int)attackValueAfterCap * 1.5;
+				if(comboBox_AntiSub.SelectedIndex == 1) {
+					// 熟練度補正
+					var airLevelWeight = 1.0;
+					airLevelWeight += Limit(comboBox_AntiSub_Level_0.SelectedIndex, 0, 7) * 0.2 / 7;
+					airLevelWeight += Limit(comboBox_AntiSub_Level_1.SelectedIndex, 0, 7) * 0.1 / 7;
+					airLevelWeight += Limit(comboBox_AntiSub_Level_2.SelectedIndex, 0, 7) * 0.1 / 7;
+					airLevelWeight += Limit(comboBox_AntiSub_Level_3.SelectedIndex, 0, 7) * 0.1 / 7;
+					lastAttackValueWithCL_ *= airLevelWeight;
+				}
+				double lastAttackValueWithCL = (int)lastAttackValueWithCL_;
+				// ダメージを算出し、ヒストグラムを取る
+				//初期設定
+				var hist = new Dictionary<int, int>();
+				var defense = int.Parse(textBox_Defense.Text);
+				var nowHP = int.Parse(textBox_NowHP.Text);
+				var maxHP = int.Parse(textBox_MaxHP.Text);
+				double[] ammoWeight = { 1.0, 0.8, 0.4, 0.0 };
+				var ammo = ammoWeight[comboBox_AmmoPer.SelectedIndex];
+				//ループ
+				for(int i = 0; i < loopCount[comboBox_TryCount.SelectedIndex]; ++i) {
+					// ダメージ計算
+					var defenseValue = defense * 0.7 + rand.Next(defense) * 0.6;
+					int damage;
+					if(rand.NextDouble() * 1000 < slider_Critical.Value) {
+						// クリティカル
+						damage = (int)((lastAttackValueWithCL - defenseValue) * ammo);
+					} else {
+						// 非クリティカル
+						damage = (int)((lastAttackValueWithoutCL - defenseValue) * ammo);
+					}
+					//0以下の際の処理(カスダメ)
+					if(damage <= 0) {
+						damage = (int)(nowHP * 0.06 + rand.Next(nowHP) * 0.08);
+					}
+					//艦娘で、更に現在耐久以上だった際の処理(大破ストッパー)
+					if(damage >= nowHP && (bool)checkBox_Kammusu.IsChecked) {
+						damage = (int)(nowHP * 0.5 + rand.Next(nowHP) * 0.3);
+					}
+					// ヒストグラムカウント
+					if(hist.ContainsKey(damage)) {
+						++hist[damage];
+					} else {
+						hist[damage] = 1;
+					}
+				}
+				//ソート
+				sorted_hist = new SortedDictionary<int, int>(hist);
+				// ヒストグラムを描画させる
+				DrawGraph();
+			} catch { }
+		}
+
+		/// <summary>
+		/// グラフを再描画する処理(中身はサンプル)
+		/// </summary>
+		private void DrawGraph() {
+			// スケールの最大・最小値を計算する
+			double axisX_Max = double.MinValue, axisX_Min = double.MaxValue, axisY_Max = double.MinValue, axisY_Min = double.MaxValue;
+			foreach(var pair in sorted_hist) {
+				var valuePer = 100.0 * pair.Value / loopCount[comboBox_TryCount.SelectedIndex];
+				axisX_Max = Math.Max(axisX_Max, pair.Key);
+				axisX_Min = Math.Min(axisX_Min, pair.Key);
+				axisY_Max = Math.Max(axisY_Max, valuePer);
+				axisY_Min = Math.Min(axisY_Min, valuePer);
+			}
+			axisX_Min = Math.Floor(axisX_Min);
+			axisX_Max = Math.Ceiling(axisX_Max);
+			axisY_Min = Math.Floor(axisY_Min);
+			axisY_Max = Math.Ceiling(axisY_Max);
+			// チャートにグラフを追加する
+			chart.Series.Clear();
+			var seriesHist = new Series();
+			seriesHist.ChartType = SeriesChartType.Line;
+			seriesHist.MarkerStyle = MarkerStyle.Circle;
+			foreach(var pair in sorted_hist) {
+				// 値を取得しつつ、スケールの最大・最小値を計算する
+				var valuePer = 100.0 * pair.Value / loopCount[comboBox_TryCount.SelectedIndex];
+				seriesHist.Points.AddXY(pair.Key, valuePer);
+			}
+			chart.Series.Add(seriesHist);
+			chart.ChartAreas[0].AxisX.Minimum = axisX_Min;
+			chart.ChartAreas[0].AxisX.Maximum = axisX_Max;
+			chart.ChartAreas[0].AxisY.Minimum = 0.0;
+			chart.ChartAreas[0].AxisY.Maximum = axisY_Max;
+		}
+
+		/// <summary>
+		/// 「自動再計算」チェックを付けた際の処理
+		/// </summary>
+		private void checkBox_AutoCalc_Checked(object sender, RoutedEventArgs e) {
+			autoCalcFlg = true;
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		/// <summary>
+		/// 「自動再計算」チェックを外した際の処理
+		/// </summary>
+		private void checkBox_AutoCalc_Unchecked(object sender, RoutedEventArgs e) {
+			autoCalcFlg = false;
+		}
+
+		/// <summary>
+		/// 値を制限する
+		/// </summary>
+		private int Limit(int a, int min, int max) {
+			if(a < min)
+				return min;
+			if(a > max)
+				return max;
+			return a;
+		}
+
+		private void comboBox_Position_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void comboBox_Formation_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void comboBox_Damage_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void comboBox_AmmoPer_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void textBox_AntiSub_Kammusu_TextChanged(object sender, TextChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void textBox_AntiSub_Weapons_TextChanged(object sender, TextChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void comboBox_AntiSub_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void checkBox_AntiSubSynergy_Checked(object sender, RoutedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void checkBox_AntiSubSynergy_Unchecked(object sender, RoutedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void comboBox_AntiSub_Level_0_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void comboBox_AntiSub_Level_1_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void comboBox_AntiSub_Level_2_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void comboBox_AntiSub_Level_3_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void textBox_Defense_TextChanged(object sender, TextChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void textBox_MaxHP_TextChanged(object sender, TextChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void textBox_NowHP_TextChanged(object sender, TextChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void checkBox_Kammusu_Checked(object sender, RoutedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void checkBox_Kammusu_Unchecked(object sender, RoutedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
+		}
+
+		private void comboBox_TryCount_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if(autoCalcFlg) {
+				CalcHistogram();
+			}
 		}
 	}
 }
