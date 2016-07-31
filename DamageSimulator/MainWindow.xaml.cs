8ba254d66e94429598f20d7ea8cd366a20e1355f
YSRKEN <ysr.ken@gmail.com>
2016-07-31 19:13:16 +0900

    「彩雲有り」「彩雲無し」に対応

--- DamageSimulator/MainWindow.xaml.cs
+++ DamageSimulator/MainWindow.xaml.cs
@@ -61,136 +61,167 @@ namespace DamageSimulator {
 		}
 
 		/// <summary>
+		/// 計算処理(分割後)
+		/// </summary>
+		private void CalcDataSlave(Dictionary<int, int> hist, int[] status, int type, int loops) {
+			// 基本攻撃力を算出する
+			var baseAttackValue = 0.0;
+			if(tabControl.SelectedIndex == TabIndexAntiSub) {
+				// 素対潜
+				baseAttackValue += Math.Sqrt(int.Parse(textBox_AntiSub_Kammusu.Text)) * 2;
+				// 装備対潜
+				baseAttackValue += 1.5 * int.Parse(textBox_AntiSub_Weapons.Text);
+				// 装備改修値
+				if(comboBox_AntiSub.SelectedIndex == 0) {
+					baseAttackValue += Math.Sqrt(comboBox_AntiSub_Level_0.SelectedIndex);
+					baseAttackValue += Math.Sqrt(comboBox_AntiSub_Level_1.SelectedIndex);
+					baseAttackValue += Math.Sqrt(comboBox_AntiSub_Level_2.SelectedIndex);
+					baseAttackValue += Math.Sqrt(comboBox_AntiSub_Level_3.SelectedIndex);
+				}
+				// 対潜定数
+				baseAttackValue += (comboBox_AntiSub.SelectedIndex == 0 ? 13 : 8);
+			}
+			// キャップ前攻撃力を出す
+			var attackValueBeforeCap = baseAttackValue;
+			//対潜シナジー補正
+			if(tabControl.SelectedIndex == TabIndexAntiSub) {
+				if((bool)checkBox_AntiSubSynergy.IsChecked)
+					attackValueBeforeCap *= 1.15;
+			}
+			//陣形補正
+			if(tabControl.SelectedIndex == TabIndexAntiSub) {
+				double[] param = { 0.6, 0.8, 1.2, 1.0, 1.3 };
+				attackValueBeforeCap *= param[comboBox_Formation.SelectedIndex];
+			}
+			//損傷補正
+			if(tabControl.SelectedIndex == TabIndexAntiSub) {
+				double[] param = { 1, 0.7, 0.4 };
+				attackValueBeforeCap *= param[comboBox_Damage.SelectedIndex];
+			}
+			//交戦形態補正
+			{
+				double[] param = { 1, 0.8, 1.2, 0.6 };
+				attackValueBeforeCap *= param[type];
+			}
+			// キャップ後攻撃力を出す
+			var attackValueAfterCap = attackValueBeforeCap;
+			if(tabControl.SelectedIndex == TabIndexAntiSub) {
+				if(attackValueAfterCap > 100.0)
+					attackValueAfterCap = 100.0 + Math.Sqrt(attackValueAfterCap - 100.0);
+			}
+			// 最終攻撃力を出す
+			double lastAttackValueWithoutCL = (int)attackValueAfterCap;
+			var lastAttackValueWithCL_ = (int)attackValueAfterCap * 1.5;
+			if(comboBox_AntiSub.SelectedIndex == 1) {
+				// 熟練度補正
+				var airLevelWeight = 1.0;
+				airLevelWeight += Limit(comboBox_AntiSub_Level_0.SelectedIndex, 0, 7) * 0.2 / 7;
+				airLevelWeight += Limit(comboBox_AntiSub_Level_1.SelectedIndex, 0, 7) * 0.1 / 7;
+				airLevelWeight += Limit(comboBox_AntiSub_Level_2.SelectedIndex, 0, 7) * 0.1 / 7;
+				airLevelWeight += Limit(comboBox_AntiSub_Level_3.SelectedIndex, 0, 7) * 0.1 / 7;
+				lastAttackValueWithCL_ *= airLevelWeight;
+			}
+			double lastAttackValueWithCL = (int)lastAttackValueWithCL_;
+			// ダメージを算出し、ヒストグラムを取る
+			//初期設定
+			var defense = int.Parse(textBox_Defense.Text);
+			var nowHP = int.Parse(textBox_NowHP.Text);
+			var maxHP = int.Parse(textBox_MaxHP.Text);
+			double[] ammoWeight = { 1.0, 0.8, 0.4, 0.0 };
+			var ammo = ammoWeight[comboBox_AmmoPer.SelectedIndex];
+			//ループ
+			for(int i = 0; i < loops; ++i) {
+				// ダメージ計算
+				var defenseValue = defense * 0.7 + rand.Next(defense) * 0.6;
+				int damage;
+				if(rand.NextDouble() * 1000 < slider_Critical.Value) {
+					// クリティカル
+					damage = (int)((lastAttackValueWithCL - defenseValue) * ammo);
+				} else {
+					// 非クリティカル
+					damage = (int)((lastAttackValueWithoutCL - defenseValue) * ammo);
+				}
+				//0以下の際の処理(カスダメ)
+				if(damage <= 0) {
+					damage = (int)(nowHP * 0.06 + rand.Next(nowHP) * 0.08);
+				}
+				//艦娘で、更に現在耐久以上だった際の処理(大破ストッパー)
+				if(damage >= nowHP && (bool)checkBox_Kammusu.IsChecked) {
+					//大破：最大耐久の25％「以下」
+					if(nowHP * 4 > maxHP) {
+						damage = (int)(nowHP * 0.5 + rand.Next(nowHP) * 0.3);
+					}
+				}
+				// ヒストグラムカウント
+				if(hist.ContainsKey(damage)) {
+					++hist[damage];
+				} else {
+					hist[damage] = 1;
+				}
+				// 状態カウント
+				var nowHP2 = nowHP - damage;
+				if(nowHP == nowHP2) {
+					++status[0];
+				} else if(nowHP2 * 4 > maxHP) {
+					++status[1];
+				} else if(nowHP2 * 2 > maxHP) {
+					++status[2];
+				} else if(nowHP2 * 4 > maxHP) {
+					++status[3];
+				} else if(nowHP2 > 0) {
+					++status[4];
+				} else {
+					++status[5];
+				}
+			}
+		}
+
+		/// <summary>
+		/// 計算処理
+		/// </summary>
+		private void CalcData() {
+			// 「彩雲有り」「彩雲無し」のため、処理を分割する
+			var hist = new Dictionary<int, int>();	//ヒストグラム(元)
+			int[] status = { 0, 0, 0, 0, 0, 0 };    //状態カウント
+			//彩雲が絡む場合、確率毎に按分する
+			//「彩雲有り」の場合、同航戦：反航戦：丁字有利：丁字不利＝45：40：15：0
+			//「彩雲無し」の場合、同航戦：反航戦：丁字有利：丁字不利＝45：30：15：10
+			var count = loopCount[comboBox_TryCount.SelectedIndex];
+			switch(comboBox_Position.SelectedIndex) {
+			case 4:
+				CalcDataSlave(hist, status, 0, count * 45 / 100);
+				CalcDataSlave(hist, status, 1, count * 40 / 100);
+				CalcDataSlave(hist, status, 2, count * 15 / 100);
+				break;
+			case 5:
+				CalcDataSlave(hist, status, 0, count * 45 / 100);
+				CalcDataSlave(hist, status, 1, count * 30 / 100);
+				CalcDataSlave(hist, status, 2, count * 15 / 100);
+				CalcDataSlave(hist, status, 3, count * 10 / 100);
+				break;
+			default:
+				CalcDataSlave(hist, status, comboBox_Position.SelectedIndex, count);
+				break;
+			}
+			//ソート
+			sorted_hist = new SortedDictionary<int, int>(hist);
+			//状態messageの更新
+			status_message = "無傷率：" + Math.Round(100.0 * status[0] / count, 1) + "%\n";
+			status_message += "カスダメ率：" + Math.Round(100.0 * status[1] / count, 1) + "%\n";
+			status_message += "小破率：" + Math.Round(100.0 * status[2] / count, 1) + "%\n";
+			status_message += "中破率：" + Math.Round(100.0 * status[3] / count, 1) + "%\n";
+			status_message += "大破率：" + Math.Round(100.0 * status[4] / count, 1) + "%\n";
+			status_message += "撃沈率：" + Math.Round(100.0 * status[5] / count, 1) + "%";
+			DataContext = new { CriticalLabelString = critical_label, StatusMessageString = status_message };
+		}
+
+		/// <summary>
 		/// ヒストグラムの計算処理
 		/// </summary>
 		private void CalcHistogram() {
 			try {
-				// 基本攻撃力を算出する
-				var baseAttackValue = 0.0;
-				if(tabControl.SelectedIndex == TabIndexAntiSub) {
-					// 素対潜
-					baseAttackValue += Math.Sqrt(int.Parse(textBox_AntiSub_Kammusu.Text)) * 2;
-					// 装備対潜
-					baseAttackValue += 1.5 * int.Parse(textBox_AntiSub_Weapons.Text);
-					// 装備改修値
-					if(comboBox_AntiSub.SelectedIndex == 0) {
-						baseAttackValue += Math.Sqrt(comboBox_AntiSub_Level_0.SelectedIndex);
-						baseAttackValue += Math.Sqrt(comboBox_AntiSub_Level_1.SelectedIndex);
-						baseAttackValue += Math.Sqrt(comboBox_AntiSub_Level_2.SelectedIndex);
-						baseAttackValue += Math.Sqrt(comboBox_AntiSub_Level_3.SelectedIndex);
-					}
-					// 対潜定数
-					baseAttackValue += (comboBox_AntiSub.SelectedIndex == 0 ? 13 : 8);
-				}
-				// キャップ前攻撃力を出す
-				var attackValueBeforeCap = baseAttackValue;
-				//対潜シナジー補正
-				if(tabControl.SelectedIndex == TabIndexAntiSub) {
-					if((bool)checkBox_AntiSubSynergy.IsChecked)
-						attackValueBeforeCap *= 1.15;
-				}
-				//陣形補正
-				if(tabControl.SelectedIndex == TabIndexAntiSub) {
-					double[] param = { 0.6, 0.8, 1.2, 1.0, 1.3 };
-					attackValueBeforeCap *= param[comboBox_Formation.SelectedIndex];
-				}
-				//損傷補正
-				if(tabControl.SelectedIndex == TabIndexAntiSub) {
-					double[] param = { 1, 0.7, 0.4 };
-					attackValueBeforeCap *= param[comboBox_Damage.SelectedIndex];
-				}
-				//交戦形態補正
-				{
-					// 「彩雲有り」の場合、同航戦：反航戦：丁字有利：丁字不利＝45：40：15：0
-					// 「彩雲無し」の場合、同航戦：反航戦：丁字有利：丁字不利＝45：30：15：10
-					// したがって、
-					double[] param = { 1, 0.8, 1.2, 0.6 };
-					attackValueBeforeCap *= param[comboBox_Position.SelectedIndex];
-				}
-				// キャップ後攻撃力を出す
-				var attackValueAfterCap = attackValueBeforeCap;
-				if(tabControl.SelectedIndex == TabIndexAntiSub) {
-					if(attackValueAfterCap > 100.0)
-						attackValueAfterCap = 100.0 + Math.Sqrt(attackValueAfterCap - 100.0);
-				}
-				// 最終攻撃力を出す
-				double lastAttackValueWithoutCL = (int)attackValueAfterCap;
-				var lastAttackValueWithCL_ = (int)attackValueAfterCap * 1.5;
-				if(comboBox_AntiSub.SelectedIndex == 1) {
-					// 熟練度補正
-					var airLevelWeight = 1.0;
-					airLevelWeight += Limit(comboBox_AntiSub_Level_0.SelectedIndex, 0, 7) * 0.2 / 7;
-					airLevelWeight += Limit(comboBox_AntiSub_Level_1.SelectedIndex, 0, 7) * 0.1 / 7;
-					airLevelWeight += Limit(comboBox_AntiSub_Level_2.SelectedIndex, 0, 7) * 0.1 / 7;
-					airLevelWeight += Limit(comboBox_AntiSub_Level_3.SelectedIndex, 0, 7) * 0.1 / 7;
-					lastAttackValueWithCL_ *= airLevelWeight;
-				}
-				double lastAttackValueWithCL = (int)lastAttackValueWithCL_;
-				// ダメージを算出し、ヒストグラムを取る
-				//初期設定
-				var hist = new Dictionary<int, int>();
-				int[] status = { 0, 0, 0, 0, 0, 0 };
-				var defense = int.Parse(textBox_Defense.Text);
-				var nowHP = int.Parse(textBox_NowHP.Text);
-				var maxHP = int.Parse(textBox_MaxHP.Text);
-				double[] ammoWeight = { 1.0, 0.8, 0.4, 0.0 };
-				var ammo = ammoWeight[comboBox_AmmoPer.SelectedIndex];
-				//ループ
-				var count = loopCount[comboBox_TryCount.SelectedIndex];
-				for(int i = 0; i < count; ++i) {
-					// ダメージ計算
-					var defenseValue = defense * 0.7 + rand.Next(defense) * 0.6;
-					int damage;
-					if(rand.NextDouble() * 1000 < slider_Critical.Value) {
-						// クリティカル
-						damage = (int)((lastAttackValueWithCL - defenseValue) * ammo);
-					} else {
-						// 非クリティカル
-						damage = (int)((lastAttackValueWithoutCL - defenseValue) * ammo);
-					}
-					//0以下の際の処理(カスダメ)
-					if(damage <= 0) {
-						damage = (int)(nowHP * 0.06 + rand.Next(nowHP) * 0.08);
-					}
-					//艦娘で、更に現在耐久以上だった際の処理(大破ストッパー)
-					if(damage >= nowHP && (bool)checkBox_Kammusu.IsChecked) {
-						//大破：最大耐久の25％「以下」
-						if(nowHP * 4 > maxHP) {
-							damage = (int)(nowHP * 0.5 + rand.Next(nowHP) * 0.3);
-						}
-					}
-					// ヒストグラムカウント
-					if(hist.ContainsKey(damage)) {
-						++hist[damage];
-					} else {
-						hist[damage] = 1;
-					}
-					// 状態カウント
-					var nowHP2 = nowHP - damage;
-					if(nowHP == nowHP2) {
-						++status[0];
-					}else if(nowHP2 * 4 > maxHP) {
-						++status[1];
-					}else if(nowHP2 * 2 > maxHP) {
-						++status[2];
-					}else if(nowHP2 * 4 > maxHP) {
-						++status[3];
-					}else if(nowHP2 > 0) {
-						++status[4];
-					}else {
-						++status[5];
-					}
-				}
-				//ソート
-				sorted_hist = new SortedDictionary<int, int>(hist);
-				//状態messageの更新
-				status_message = "無傷率：" + Math.Round(100.0 * status[0] / count, 1) + "%\n";
-				status_message += "カスダメ率：" + Math.Round(100.0 * status[1] / count, 1) + "%\n";
-				status_message += "小破率：" + Math.Round(100.0 * status[2] / count, 1) + "%\n";
-				status_message += "中破率：" + Math.Round(100.0 * status[3] / count, 1) + "%\n";
-				status_message += "大破率：" + Math.Round(100.0 * status[4] / count, 1) + "%\n";
-				status_message += "撃沈率：" + Math.Round(100.0 * status[5] / count, 1) + "%";
-				DataContext = new { CriticalLabelString = critical_label, StatusMessageString = status_message };
+				// 計算処理
+				CalcData();
 				// ヒストグラムを描画させる
 				DrawGraph();
 			} catch { }
